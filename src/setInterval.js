05365a2531e98edbd30867c8c6f03218506e2735
mdiabate <madiabate@gmail.com>
2019-03-08 08:42:06 -0500

    Create setInterval.js

---
+++ src/setInterval.js
@@ -0,0 +1,6 @@
+var http = require("http");
+setInterval(function() {
+    http.get("http://oga-gort1.herokuapp.com");
+    http.get("http://warm-lake-53440.herokuapp.com");
+    http.get("http://afternoon-ridge-59499.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
