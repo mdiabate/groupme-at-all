ac7fa1a57434aebd6aa4ed617c030f2b0dc1880c
mdiabate <madiabate@gmail.com>
2019-03-09 22:54:48 -0500

    Update setInterval.js

--- src/setInterval.js
+++ src/setInterval.js
@@ -3,5 +3,5 @@ setInterval(function() {
     http.get("http://oga-gort1.herokuapp.com");
     http.get("http://warm-lake-53440.herokuapp.com");
     http.get("http://afternoon-ridge-59499.herokuapp.com");
-    http.get("https://thawing-beach-87774.herokuapp.com");
+    http.get("http://thawing-beach-87774.herokuapp.com");
 }, 300000); // every 5 minutes (300000)
