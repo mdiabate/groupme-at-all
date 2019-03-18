39789184a79768126940f7a002d566a393540eda
mdiabate <madiabate@gmail.com>
2019-03-17 20:30:33 -0700

    Update setInterval.js

--- src/setInterval.js
+++ src/setInterval.js
@@ -3,6 +3,7 @@ setInterval(function() {
     http.get("https://colombia-dg.herokuapp.com/");
     http.get("https://cancun-dg.herokuapp.com/");
     http.get("https://haywire.herokuapp.com/");
+    http.get("https://akisan.herokuapp.com/")
     //http.get("http://oga-gort1.herokuapp.com"); -- AKISAN
     //http.get("http://warm-lake-53440.herokuapp.com"); -- COLOMBIA
     //http.get("http://afternoon-ridge-59499.herokuapp.com"); -- CANCUN
