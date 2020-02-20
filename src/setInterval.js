ef15c40f8c0ba411f5b316ab0d4f8b2847bf2d82
mdiabate <madiabate@gmail.com>
2020-02-19 20:40:36 -0800

    Update setInterval.js

--- src/setInterval.js
+++ src/setInterval.js
@@ -3,8 +3,11 @@ setInterval(function() {
     //http.get("https://sgp19.herokuapp.com/");
     //http.get("https://lasgidi.herokuapp.com/");
     http.get("http://afronationpr.herokuapp.com/");
+  	http.get("http://dgpanama20.herokuapp.com/");
     //http.get("https://akisan.herokuapp.com/");
     //http.get("http://warm-lake-53440.herokuapp.com"); -- COLOMBIA
     //http.get("http://afternoon-ridge-59499.herokuapp.com"); -- CANCUN
     //http.get("http://thawing-beach-87774.herokuapp.com"); -- HAYWIRE
-}, 600000); // every 15 minutes (300000)
+}, 900000); // every 15 minutes (300000)
+
+setInterval(function(){http.get("http://dgpanama20.herokuapp.com/");}, 1200000);
