87ff7cb2767786cb8390221c9171fe1bc10b32e8
mdiabate <madiabate@gmail.com>
2021-08-23 10:56:46 -0700

    curacao

--- src/setInterval.js
+++ src/setInterval.js
@@ -15,5 +15,6 @@ setInterval(function(){http.get("http://dgcartagena2021.herokuapp.com/")}, 18000
 setInterval(function(){http.get("https://yachtnik.herokuapp.com/")}, 1800000);
 setInterval(function(){http.get("https://sgp19.herokuapp.com/")}, 1800000);
 setInterval(function(){http.get("https://thecaptainyachtweek.herokuapp.com/")},1800000);
+setInterval(function(){http.get("https://curacao21.herokuapp.com/.herokuapp.com/")},1800000);
 //setInterval(function(){http.get("http://dglaborday2020.herokuapp.com//")}, 1800000);
 
