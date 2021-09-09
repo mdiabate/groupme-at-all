2d423c7fdedcfbe76297768cae0dd1d69fb43e24
mdiabate <madiabate@gmail.com>
2021-09-09 13:51:43 -0400

    Update setInterval.js

--- src/setInterval.js
+++ src/setInterval.js
@@ -10,11 +10,11 @@ var http = require("http");
     //http.get("http://thawing-beach-87774.herokuapp.com"); -- HAYWIRE
 //}, 1800000); // every 15 minutes (300000)
 
-//setInterval(function(){http.get("http://lasgiditwo.herokuapp.com/")}, 1800000);
-setInterval(function(){http.get("http://dgcartagena2021.herokuapp.com/")}, 1800000);
-setInterval(function(){http.get("https://yachtnik.herokuapp.com/")}, 1800000);
+setInterval(function(){http.get("http://lasgiditwo.herokuapp.com/")}, 1800000);
+//setInterval(function(){http.get("http://dgcartagena2021.herokuapp.com/")}, 1800000);
+//setInterval(function(){http.get("https://yachtnik.herokuapp.com/")}, 1800000);
 setInterval(function(){http.get("https://sgp19.herokuapp.com/")}, 1800000);
-setInterval(function(){http.get("https://thecaptainyachtweek.herokuapp.com/")},1800000);
+//setInterval(function(){http.get("https://thecaptainyachtweek.herokuapp.com/")},1800000);
 setInterval(function(){http.get("https://curacao21.herokuapp.com/.herokuapp.com/")},1800000);
-//setInterval(function(){http.get("http://dglaborday2020.herokuapp.com//")}, 1800000);
+setInterval(function(){http.get("http://afrogreeks.herokuapp.com//")}, 1800000);
 
