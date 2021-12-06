aece64d54f5b984c9c828cc970988f4731ef1496
mdiabate <madiabate@gmail.com>
2021-12-06 03:23:01 +0100

    Update setInterval.js

--- src/setInterval.js
+++ src/setInterval.js
@@ -15,6 +15,6 @@ setInterval(function(){http.get("http://lasgiditwo.herokuapp.com/")}, 1800000);
 //setInterval(function(){http.get("https://yachtnik.herokuapp.com/")}, 1800000);
 setInterval(function(){http.get("https://sgp19.herokuapp.com/")}, 1800000);
 setInterval(function(){http.get("https://africanfamilly.herokuapp.com/")},1800000);
-setInterval(function(){http.get("https://curacao21.herokuapp.com/.herokuapp.com/")},1800000);
+setInterval(function(){http.get("https://dgwestafrica2021.herokuapp.com/")},1800000);
 setInterval(function(){http.get("http://afrogreeks.herokuapp.com//")}, 1800000);
 
