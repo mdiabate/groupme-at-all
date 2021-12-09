410cd641cd0f8259b87bb4a5b5f75dd5c9951546
mdiabate <madiabate@gmail.com>
2021-12-09 22:03:24 +0100

    Update setInterval.js

--- src/setInterval.js
+++ src/setInterval.js
@@ -10,11 +10,11 @@ var http = require("http");
     //http.get("http://thawing-beach-87774.herokuapp.com"); -- HAYWIRE
 //}, 1800000); // every 15 minutes (300000)
 
+setInterval(function(){http.get("https://dgwestafrica2021.herokuapp.com/")},1800000);
 setInterval(function(){http.get("http://lasgiditwo.herokuapp.com/")}, 1800000);
 //setInterval(function(){http.get("http://dgcartagena2021.herokuapp.com/")}, 1800000);
 //setInterval(function(){http.get("https://yachtnik.herokuapp.com/")}, 1800000);
 setInterval(function(){http.get("https://sgp19.herokuapp.com/")}, 1800000);
-setInterval(function(){http.get("https://africanfamilly.herokuapp.com/")},1800000);
-setInterval(function(){http.get("https://dgwestafrica2021.herokuapp.com/")},1800000);
-setInterval(function(){http.get("http://afrogreeks.herokuapp.com//")}, 1800000);
+//setInterval(function(){http.get("https://africanfamilly.herokuapp.com/")},1800000);
+//setInterval(function(){http.get("http://afrogreeks.herokuapp.com//")}, 1800000);
 
