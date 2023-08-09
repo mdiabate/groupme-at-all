a2af9b113754080781c4b7bd56cbcc0d7555f1aa
mdiabate <madiabate@gmail.com>
2023-08-08 23:51:10 -0400

    Update all.js

--- src/all.js
+++ src/all.js
@@ -15,65 +15,119 @@ if (!room_id || !bot_id || !token) {
   process.exit(1);
 }
 
-class AllBot {
+class WelcomeBot {
   constructor(robot) {
     this.robot = robot;
     this.blacklist = [];
+    this.welcomeMessageSent = false;
 
     this.robot.brain.once("loaded", this.loadBlacklist.bind(this));
+    this.robot.brain.once("loaded", this.loadWelcomeMessageStatus.bind(this));
 
-    // Listen for new users joining the chat
-    this.robot.enter(res => {
-      this.sendWelcomeMessage(res.envelope.user);
-    });
+    this.robot.enter(res => this.handleNewUser(res.message.user.id));
   }
 
-  // ... (other methods)
+  saveBlacklist() {
+    console.log("Saving blacklist");
+    this.robot.brain.set("blacklist", this.blacklist);
+    this.robot.brain.save();
+  }
+
+  loadBlacklist() {
+    this.blacklist = this.robot.brain.get("blacklist");
+    if (this.blacklist) console.log("Blacklist loaded successfully.");
+    else console.warn("Failed to load blacklist.");
+  }
+
+  addToBlacklist(item) {
+    this.blacklist.push(item);
+    this.saveBlacklist();
+  }
+
+  removeFromBlacklist(item) {
+    let index = this.blacklist.indexOf(item);
+    if (index !== -1) {
+      this.blacklist.splice(index, 1);
+      this.saveBlacklist();
+      console.log(`Successfully removed ${item} from blacklist.`);
+    } else {
+      console.warn(`Unable to find ${item} in blacklist!`);
+    }
+  }
+
+  getUserByName(_name) {
+    let name = _name.trim();
+    if (name[0] == "@") {
+      name = name.slice(1);
+    }
+    let user = this.robot.brain.userForName(name);
+    if (!user.user_id) return null;
+    else return user;
+  }
+
+  getUserById(id) {
+    let user = this.robot.brain.userForId(id);
+    if (!user.user_id) return null;
+    else return user;
+  }
 
   sendWelcomeMessage(user) {
-    const welcomeMessage = `Welcome, ${user.name}! We're excited about this trip to Lisbon but please remember to fill the forms in the portal if you haven't done so`;
-    this.sendMessageToGroup(welcomeMessage);
-  }
+    const welcomeMessage = `Welcome to the DG Portugal, ${user.name}! If you haven't done so yet, please fill out the flight and meal forms in the portal.`;
 
-  sendMessageToGroup(message) {
-    const payload = {
-      text: message,
-      bot_id,
+    const message = {
+      text: welcomeMessage,
+      bot_id
     };
 
-    const requestOptions = {
+    const json = JSON.stringify(message);
+    const groupmeAPIOptions = {
+      agent: false,
+      host: "api.groupme.com",
+      path: "/v3/bots/post",
+      port: 443,
       method: "POST",
       headers: {
+        "Content-Length": json.length,
         "Content-Type": "application/json",
-        "X-Access-Token": token,
-      },
-    };
-
-    const req = https.request(
-      `https://api.groupme.com/v3/groups/${room_id}/messages`,
-      requestOptions,
-      response => {
-        let data = "";
-        response.on("data", chunk => (data += chunk));
-        response.on("end", () =>
-          console.log(`[GROUPME RESPONSE] ${response.statusCode} ${data}`)
-        );
+        "X-Access-Token": token
       }
-    );
+    };
+    const req = https.request(groupmeAPIOptions, response => {
+      let data = "";
+      response.on("data", chunk => (data += chunk));
+      response.on("end", () =>
+        console.log(`[GROUPME RESPONSE] ${response.statusCode} ${data}`)
+      );
+    });
+    req.end(json);
+  }
 
-    req.write(JSON.stringify(payload));
-    req.end();
+  handleNewUser(userId) {
+    if (!this.welcomeMessageSent && this.blacklist.indexOf(userId) === -1) {
+      this.sendWelcomeMessage(this.getUserById(userId));
+      this.welcomeMessageSent = true;
+    }
+  }
+
+  respondToWelcome(res) {
+    const user = res.message.user.id;
+    this.sendWelcomeMessage(this.getUserById(user));
   }
 
   run() {
-    // ... (other listeners)
+    this.robot.hear(/get id (.+)/i, res => this.respondToID(res, res.match[1]));
+    this.robot.hear(/get name (.+)/i, res => this.respondToName(res, res.match[1]));
+    this.robot.hear(/view( raw)* blacklist/i, res => this.respondToViewBlacklist(res));
+    this.robot.hear(/blacklist (.+)/i, res => this.respondToBlacklist(res, res.match[1]));
+    this.robot.hear(/whitelist (.+)/i, res => this.respondToWhitelist(res, res.match[1]));
 
-    // Mention @all command
     this.robot.hear(/(.*)@all(.*)/i, res => this.respondToAtAll(res));
+    
+    this.robot.hear(/welcome/i, res => this.respondToWelcome(res)); // New listener for welcome command
   }
 }
 
 module.exports = robot => {
-  const bot = new AllBot(robot);
+  const bot = new WelcomeBot(robot);
   bot.run();
 };
