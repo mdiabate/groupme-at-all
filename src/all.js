9c698017c97ba24dec129f1bc6bb706e134434f8
mdiabate <madiabate@gmail.com>
2023-08-21 14:45:19 -0400

    Update all.js
    refractored

--- src/all.js
+++ src/all.js
@@ -1,6 +1,5 @@
 const https = require("https");
 
-// Bot configs read in from environment
 const bot_id = process.env.HUBOT_GROUPME_BOT_ID;
 const token = process.env.HUBOT_GROUPME_TOKEN;
 
@@ -17,48 +16,74 @@ if (!bot_id || !token) {
 class AllBot {
   constructor(robot) {
     this.robot = robot;
+    this.commands = [
+      { regex: /get id (.+)/i, handler: this.respondToID },
+      { regex: /get name (.+)/i, handler: this.respondToName },
+      { regex: /@all/i, handler: this.respondToAtAll },
+      { regex: /@itinerary/i, handler: this.respondToSchedule },
+      { regex: /@today/i, handler: this.respondToTodaySchedule },
+      { regex: /@form/i, handler: this.respondToTravelForm },
+      { regex: /@hotel/i, handler: this.respondToHotel },
+      { regex: /@lookbook/i, handler: this.respondToLookbook },
+    ];
   }
 
   getUserByName(_name) {
     let name = _name.trim();
-    if (name[0] == "@") {
+    if (name.startsWith("@")) {
       name = name.slice(1);
     }
-    let user = this.robot.brain.userForName(name);
-    if (!user.user_id) return null;
-    else return user;
+    return this.robot.brain.userForName(name);
+  }
+
+  sendMessage(message) {
+    const json = JSON.stringify(message);
+    const groupmeAPIOptions = {
+      agent: false,
+      host: "api.groupme.com",
+      path: "/v3/bots/post",
+      port: 443,
+      method: "POST",
+      headers: {
+        "Content-Length": json.length,
+        "Content-Type": "application/json",
+        "X-Access-Token": token,
+      },
+    };
+    const req = https.request(groupmeAPIOptions, (response) => {
+      let data = "";
+      response.on("data", (chunk) => (data += chunk));
+      response.on("end", () =>
+        console.log(`[GROUPME RESPONSE] ${response.statusCode} ${data}`)
+      );
+    });
+    req.end(json);
   }
 
   respondToID(res, target) {
-    console.log(`Looking for user ID by name: ${target}`);
     const found = this.getUserByName(target);
 
     if (found) {
       const id = found.user_id;
-      console.log(`Found ID ${id} by name ${target}`);
-      res.send(`${target}: ${id}`);
+      this.sendMessage({ text: `${target}: ${id}`, bot_id });
     } else {
-      res.send(`Could not find a user with the name ${target}`);
+      this.sendMessage({ text: `Could not find a user with the name ${target}`, bot_id });
     }
   }
 
   respondToName(res, target) {
-    console.log(`Looking for user name by ID: ${target}`);
     const found = this.robot.brain.userForId(target);
 
     if (found) {
       const name = found.name;
-      console.log(`Found name ${name} by ID ${target}`);
-      res.send(`${target}: ${name}`);
+      this.sendMessage({ text: `${target}: ${name}`, bot_id });
     } else {
-      res.send(`Could not find a user with the ID ${target}`);
+      this.sendMessage({ text: `Could not find a user with the ID ${target}`, bot_id });
     }
   }
 
   respondToAtAll(res) {
-    const text =
-      res.match[0].length > res.match[1].length ? res.match[0] : res.match[1];
-
+    const text = res.match[0].length > res.match[1].length ? res.match[0] : res.match[1];
     const message = {
       text,
       bot_id,
@@ -71,34 +96,44 @@ class AllBot {
       message.attachments[0].user_ids.push(userID);
     });
 
-    const json = JSON.stringify(message);
-    const groupmeAPIOptions = {
-      agent: false,
-      host: "api.groupme.com",
-      path: "/v3/bots/post",
-      port: 443,
-      method: "POST",
-      headers: {
-        "Content-Length": json.length,
-        "Content-Type": "application/json",
-        "X-Access-Token": token,
-      },
-    };
-    const req = https.request(groupmeAPIOptions, (response) => {
-      let data = "";
-      response.on("data", (chunk) => (data += chunk));
-      response.on("end", () =>
-        console.log(`[GROUPME RESPONSE] ${response.statusCode} ${data}`)
-      );
-    });
-    req.end(json);
+    this.sendMessage(message);
   }
 
   respondToSchedule(res) {
     const schedule = "Here's the schedule for the trip:\n1. Welcome Mixer and Night Swim \n2. DG Wine Tour \n3. Opulence Dinner \n4. Sunset Boatride ";
 
+    this.sendGroupMessageWithMentions(schedule);
+  }
+
+  respondToTodaySchedule(res) {
+    const todayFormatted = new Date().toISOString().split("T")[0];
+    const schedules = {
+      '2023-08-20': 'Here is the schedule for Day 1:\n1. Event A\n2. Event B\n3. Event C',
+      '2023-08-05': 'Here is the schedule for Day 2:\n1. Event X\n2. Event Y\n3. Event Z',
+    };
+    const schedule = schedules[todayFormatted] || 'There is no schedule available for today.';
+
+    this.sendGroupMessageWithMentions(schedule);
+  }
+
+  respondToTravelForm(res) {
+    const travelFormURL = "https://docs.google.com/forms/d/e/1FAIpQLSf9tJWxnH0NPq_oFB_a_bWwZq0zqf1AWDcBZ4tnvnxIsZB3qg/viewform";
+    this.sendMessage({ text: `Here's the link to the flight form: ${travelFormURL}`, bot_id });
+  }
+
+  respondToHotel(res) {
+    const hotelInfo = "Lisbon Marriott Hotel\nAddress: Avenida dos Combatentes 45, Lisbon 1600-042, Portugal";
+    this.sendMessage({ text: `Here's the information about Lisbon Marriott Hotel:\n${hotelInfo}`, bot_id });
+  }
+
+  respondToLookbook(res) {
+    const lookbookUrl = "https://drive.google.com/file/d/1WVCka6T2KG1seeskCAvYvZKrOFwusPmG/view";
+    this.sendGroupMessageWithMentions(`Here's the lookbook URL: ${lookbookUrl}`);
+  }
+
+  sendGroupMessageWithMentions(text) {
     const message = {
-      text: schedule,
+      text,
       bot_id,
       attachments: [{ loci: [], type: "mentions", user_ids: [] }],
     };
@@ -109,187 +144,13 @@ class AllBot {
       message.attachments[0].user_ids.push(userID);
     });
 
-    const json = JSON.stringify(message);
-    const groupmeAPIOptions = {
-      agent: false,
-      host: "api.groupme.com",
-      path: "/v3/bots/post",
-      port: 443,
-      method: "POST",
-      headers: {
-        "Content-Length": json.length,
-        "Content-Type": "application/json",
-        "X-Access-Token": token,
-      },
-    };
-    const req = https.request(groupmeAPIOptions, (response) => {
-      let data = "";
-      response.on("data", (chunk) => (data += chunk));
-      response.on("end", () =>
-        console.log(`[GROUPME RESPONSE] ${response.statusCode} ${data}`)
-      );
-    });
-    req.end(json);
-  }
-
-  respondToTodaySchedule(res) {
-    const today = new Date();
-    const todayFormatted = today.toISOString().split('T')[0];
-
-    const schedules = {
-      '2023-08-20': 'Here is the schedule for Day 1:\n1. Event A\n2. Event B\n3. Event C',
-      '2023-08-05': 'Here is the schedule for Day 2:\n1. Event X\n2. Event Y\n3. Event Z',
-    };
-
-    const schedule = schedules[todayFormatted] || 'There is no schedule available for today.';
-
-    const message = {
-      text: schedule,
-      bot_id,
-      attachments: [{ loci: [], type: 'mentions', user_ids: [] }],
-    };
-
-    const users = this.robot.brain.users();
-    Object.keys(users).forEach((userID, index) => {
-      message.attachments[0].loci.push([index, index + 1]);
-      message.attachments[0].user_ids.push(userID);
-    });
-
-    const json = JSON.stringify(message);
-    const groupmeAPIOptions = {
-      agent: false,
-      host: 'api.groupme.com',
-      path: '/v3/bots/post',
-      port: 443,
-      method: 'POST',
-      headers: {
-        'Content-Length': json.length,
-        'Content-Type': 'application/json',
-        'X-Access-Token': token,
-      },
-    };
-    const req = https.request(groupmeAPIOptions, (response) => {
-      let data = '';
-      response.on('data', (chunk) => (data += chunk));
-      response.on('end', () =>
-        console.log(`[GROUPME RESPONSE] ${response.statusCode} ${data}`)
-      );
-    });
-    req.end(json);
-  }
-  
-  respondToTravelForm(res) {
-    const travelFormURL = "https://docs.google.com/forms/d/e/1FAIpQLSf9tJWxnH0NPq_oFB_a_bWwZq0zqf1AWDcBZ4tnvnxIsZB3qg/viewform";
-    
-    const message = {
-      text: `Here's the link to the flight form: ${travelFormURL}`,
-      bot_id,
-    };
-
-    const json = JSON.stringify(message);
-    const groupmeAPIOptions = {
-      agent: false,
-      host: "api.groupme.com",
-      path: "/v3/bots/post",
-      port: 443,
-      method: "POST",
-      headers: {
-        "Content-Length": json.length,
-        "Content-Type": "application/json",
-        "X-Access-Token": token,
-      },
-    };
-    const req = https.request(groupmeAPIOptions, (response) => {
-      let data = "";
-      response.on("data", (chunk) => (data += chunk));
-      response.on("end", () =>
-        console.log(`[GROUPME RESPONSE] ${response.statusCode} ${data}`)
-      );
-    });
-    req.end(json);
-  }
-
-  respondToHotel(res) {
-    const hotelInfo = "Lisbon Marriott Hotel\nAddress: Avenida dos Combatentes 45, Lisbon 1600-042, Portugal";
-    
-    const message = {
-      text: `Here's the information about Lisbon Marriott Hotel:\n${hotelInfo}`,
-      bot_id,
-    };
-
-    const json = JSON.stringify(message);
-    const groupmeAPIOptions = {
-      agent: false,
-      host: "api.groupme.com",
-      path: "/v3/bots/post",
-      port: 443,
-      method: "POST",
-      headers: {
-        "Content-Length": json.length,
-        "Content-Type": "application/json",
-        "X-Access-Token": token,
-      },
-    };
-    const req = https.request(groupmeAPIOptions, (response) => {
-      let data = "";
-      response.on("data", (chunk) => (data += chunk));
-      response.on("end", () =>
-        console.log(`[GROUPME RESPONSE] ${response.statusCode} ${data}`)
-      );
-    });
-    req.end(json);
-  }
-
-  respondToLookbook(res) {
-    const lookbookUrl = "https://drive.google.com/file/d/1WVCka6T2KG1seeskCAvYvZKrOFwusPmG/view";
-
-    const message = {
-      text: `Here's the lookbook URL: ${lookbookUrl}`,
-      bot_id,
-      attachments: [{ loci: [], type: 'mentions', user_ids: [] }],
-    };
-
-    const users = this.robot.brain.users();
-    Object.keys(users).forEach((userID, index) => {
-      message.attachments[0].loci.push([index, index + 1]);
-      message.attachments[0].user_ids.push(userID);
-    });
-
-    const json = JSON.stringify(message);
-    const groupmeAPIOptions = {
-      agent: false,
-      host: 'api.groupme.com',
-      path: '/v3/bots/post',
-      port: 443,
-      method: 'POST',
-      headers: {
-        'Content-Length': json.length,
-        'Content-Type': 'application/json',
-        'X-Access-Token': token,
-      },
-    };
-    const req = https.request(groupmeAPIOptions, (response) => {
-      let data = '';
-      response.on('data', (chunk) => (data += chunk));
-      response.on('end', () =>
-        console.log(`[GROUPME RESPONSE] ${response.statusCode} ${data}`)
-      );
-    });
-    req.end(json);
+    this.sendMessage(message);
   }
 
   run() {
-    this.robot.hear(/get id (.+)/i, res => this.respondToID(res, res.match[1]));
-    this.robot.hear(/get name (.+)/i, res =>
-      this.respondToName(res, res.match[1])
-    );
-
-    this.robot.hear(/@all/i, res => this.respondToAtAll(res));
-    this.robot.hear(/@itinerary/i, res => this.respondToSchedule(res));
-    this.robot.hear(/@today/i, res => this.respondToTodaySchedule(res));
-    this.robot.hear(/@form/i, res => this.respondToTravelForm(res));
-    this.robot.hear(/@hotel/i, res => this.respondToHotel(res));
-    this.robot.hear(/@lookbook/i, res => this.respondToLookbook(res));
+    this.commands.forEach(({ regex, handler }) => {
+      this.robot.hear(regex, (res) => handler.call(this, res, res.match[1]));
+    });
   }
 }
 
