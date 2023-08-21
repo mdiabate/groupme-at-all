380822e4a763d5afbb271f21f1303571ab91982c
mdiabate <madiabate@gmail.com>
2023-08-21 09:38:07 -0400

    Update all.js
    reverted

--- src/all.js
+++ src/all.js
@@ -16,27 +16,269 @@ if (!room_id || !bot_id || !token) {
   process.exit(1);
 }
 
-class GroupMeBot {
+class AllBot {
   constructor(robot) {
     this.robot = robot;
-    this.messageDefaults = {
-      bot_id: process.env.HUBOT_GROUPME_BOT_ID,
-    };
-    this.commands = [
-      { pattern: /(.*)@all(.*)/i, handler: this.respondToAtAll },
-      { pattern: /@itinerary/i, handler: this.respondToSchedule },
-      { pattern: /@today/i, handler: this.respondToTodaySchedule },
-      { pattern: /@form/i, handler: this.respondToTravelForm },
-      { pattern: /@hotel/i, handler: this.respondToHotel },
-      { pattern: /@lookbook/i, handler: this.respondToLookbook },
-    ];
+    this.blacklist = [];
+
+    // Load the blacklist as soon as we can
+    this.robot.brain.once("loaded", this.loadBlacklist.bind(this));
   }
 
-  sendMessage(text, attachments = []) {
+  saveBlacklist() {
+    console.log("Saving blacklist");
+    this.robot.brain.set("blacklist", this.blacklist);
+    this.robot.brain.save();
+  }
+
+  loadBlacklist() {
+    this.blacklist = this.robot.brain.get("blacklist");
+    if (this.blacklist) console.log("Blacklist loaded successfully.");
+    else console.warn("Failed to load blacklist.");
+  }
+
+  addToBlacklist(item) {
+    this.blacklist.push(item);
+    this.saveBlacklist();
+  }
+
+  removeFromBlacklist(item) {
+    let index = this.blacklist.indexOf(item);
+    if (index !== -1) {
+      this.blacklist.splice(index, 1);
+      this.saveBlacklist();
+      console.log(`Successfully removed ${item} from blacklist.`);
+    } else {
+      console.warn(`Unable to find ${item} in blacklist!`);
+    }
+  }
+
+  getUserByName(_name) {
+    let name = _name.trim();
+    if (name[0] == "@") {
+      name = name.slice(1);
+    }
+    let user = this.robot.brain.userForName(name);
+    if (!user.user_id) return null;
+    else return user;
+  }
+
+  getUserById(id) {
+    let user = this.robot.brain.userForId(id);
+    if (!user.user_id) return null;
+    else return user;
+  }
+
+  respondToID(res, target) {
+    // Get ID command
+    console.log(`Looking for user ID by name: ${target}`);
+    const found = this.getUserByName(target);
+
+    if (found) {
+      const id = found.user_id;
+      console.log(`Found ID ${id} by name ${target}`);
+      res.send(`${target}: ${id}`);
+    } else {
+      res.send(`Could not find a user with the name ${target}`);
+    }
+  }
+
+  respondToName(res, target) {
+    console.log(`Looking for user name by ID: ${target}`);
+    const found = this.getUserById(target);
+
+    if (found) {
+      const name = found.name;
+      console.log(`Found name ${name} by ID ${target}`);
+      res.send(`${target}: ${name}`);
+    } else {
+      res.send(`Could not find a user with the ID ${target}`);
+    }
+  }
+
+  respondToViewBlacklist(res) {
+    // Raw blacklist
+    if (res.match[1]) return res.send(JSON.stringify(this.blacklist));
+
+    const blacklistNames = this.blacklist.map(
+      user => this.getUserById(user).name
+    );
+
+    if (blacklistNames.length > 0) return res.send(blacklistNames.join(", "));
+    else return res.send("There are currently no users blacklisted.");
+  }
+
+  respondToBlacklist(res, target) {
+    const user = this.getUserByName(target);
+
+    if (!user) return res.send(`Could not find a user with the name ${target}`);
+
+    console.log(`Blacklisting ${target}, ${user.user_id}`);
+    this.addToBlacklist(user.user_id);
+    res.send(`Blacklisted ${target} successfully.`);
+  }
+
+  respondToWhitelist(res, target) {
+    const user = this.getUserByName(target);
+
+    if (!user) return res.send(`Could not find a user with the name ${target}`);
+
+    console.log(`Whitelisting ${target}, ${user.user_id}`);
+    this.removeFromBlacklist(user.user_id);
+    res.send(`Whitelisted ${target} successfully`);
+  }
+
+  respondToAtAll(res) {
+    // Select the longer of the two options.
+    // TODO: Maybe combine them?
+    const text =
+      res.match[0].length > res.match[1].length ? res.match[0] : res.match[1];
+
+    // Default text if not long enough
+    // TODO: Is this necessary? Can't we tag everyone on a 1 character message?
+    // if (text.length < users.length)
+    //   text = "Please check the GroupMe, everyone.";
+
+    // The message for use in GroupMe API
     const message = {
       text,
-      attachments,
-      ...this.messageDefaults,
+      bot_id,
+      attachments: [{ loci: [], type: "mentions", user_ids: [] }]
+    };
+
+    
+
+    // Add "mention" for each user
+    const users = this.robot.brain.users();
+    Object.keys(users).map((userID, index) => {
+      // Skip blacklisted users
+      if (this.blacklist.indexOf(userID) !== -1) return;
+
+      // TODO: Would [i, i] work?
+      message.attachments[0].loci.push([index, index + 1]);
+      message.attachments[0].user_ids.push(userID);
+    });
+
+    // Send the request
+    const json = JSON.stringify(message);
+    const groupmeAPIOptions = {
+      agent: false,
+      host: "api.groupme.com",
+      path: "/v3/bots/post",
+      port: 443,
+      method: "POST",
+      headers: {
+        "Content-Length": json.length,
+        "Content-Type": "application/json",
+        "X-Access-Token": token
+      }
+    };
+    const req = https.request(groupmeAPIOptions, response => {
+      let data = "";
+      response.on("data", chunk => (data += chunk));
+      response.on("end", () =>
+        console.log(`[GROUPME RESPONSE] ${response.statusCode} ${data}`)
+      );
+    });
+    req.end(json);
+  }
+
+  respondToSchedule(res) {
+    // Implement your schedule fetching logic here
+    const schedule = "Here's the schedule for the trip:\n1. Welcome Mixer and Night Swim \n2. DG Wine Tour \n3. Opulence Dinner \n4. Sunset Boatride ";
+
+    const message = {
+      text: schedule,
+      bot_id,
+      attachments: [{ loci: [], type: "mentions", user_ids: [] }]
+    };
+
+    const users = this.robot.brain.users();
+    Object.keys(users).forEach((userID, index) => {
+      if (this.blacklist.indexOf(userID) !== -1) return;
+      message.attachments[0].loci.push([index, index + 1]);
+      message.attachments[0].user_ids.push(userID);
+    });
+
+    const json = JSON.stringify(message);
+    const groupmeAPIOptions = {
+      agent: false,
+      host: "api.groupme.com",
+      path: "/v3/bots/post",
+      port: 443,
+      method: "POST",
+      headers: {
+        "Content-Length": json.length,
+        "Content-Type": "application/json",
+        "X-Access-Token": token,
+      },
+    };
+    const req = https.request(groupmeAPIOptions, (response) => {
+      let data = "";
+      response.on("data", (chunk) => (data += chunk));
+      response.on("end", () =>
+        console.log(`[GROUPME RESPONSE] ${response.statusCode} ${data}`)
+      );
+    });
+    req.end(json);
+  }
+
+    respondToTodaySchedule(res) {
+    const today = new Date();
+    const todayFormatted = today.toISOString().split('T')[0]; // Get today's date in YYYY-MM-DD format
+
+    // Example schedule data for multiple days
+    const schedules = {
+      '2023-08-20': 'Here is the schedule for Day 1:\n1. Event A\n2. Event B\n3. Event C',
+      '2023-08-05': 'Here is the schedule for Day 2:\n1. Event X\n2. Event Y\n3. Event Z',
+      // Add more dates and schedules as needed
+    };
+
+    const schedule = schedules[todayFormatted] || 'There is no schedule available for today.';
+
+    const message = {
+      text: schedule,
+      bot_id,
+      attachments: [{ loci: [], type: 'mentions', user_ids: [] }],
+    };
+
+    const users = this.robot.brain.users();
+    Object.keys(users).forEach((userID, index) => {
+      if (this.blacklist.indexOf(userID) !== -1) return;
+      message.attachments[0].loci.push([index, index + 1]);
+      message.attachments[0].user_ids.push(userID);
+    });
+
+    const json = JSON.stringify(message);
+    const groupmeAPIOptions = {
+      agent: false,
+      host: 'api.groupme.com',
+      path: '/v3/bots/post',
+      port: 443,
+      method: 'POST',
+      headers: {
+        'Content-Length': json.length,
+        'Content-Type': 'application/json',
+        'X-Access-Token': token,
+      },
+    };
+    const req = https.request(groupmeAPIOptions, (response) => {
+      let data = '';
+      response.on('data', (chunk) => (data += chunk));
+      response.on('end', () =>
+        console.log(`[GROUPME RESPONSE] ${response.statusCode} ${data}`)
+      );
+    });
+    req.end(json);
+  }
+  
+  
+  respondToTravelForm(res) {
+    const travelFormURL = "https://docs.google.com/forms/d/e/1FAIpQLSf9tJWxnH0NPq_oFB_a_bWwZq0zqf1AWDcBZ4tnvnxIsZB3qg/viewform";
+    
+    const message = {
+      text: `Here's the link to the flight form: ${travelFormURL}`,
+      bot_id,
     };
 
     const json = JSON.stringify(message);
@@ -49,10 +291,9 @@ class GroupMeBot {
       headers: {
         "Content-Length": json.length,
         "Content-Type": "application/json",
-        "X-Access-Token": process.env.HUBOT_GROUPME_TOKEN,
+        "X-Access-Token": token,
       },
     };
-
     const req = https.request(groupmeAPIOptions, (response) => {
       let data = "";
       response.on("data", (chunk) => (data += chunk));
@@ -63,59 +304,105 @@ class GroupMeBot {
     req.end(json);
   }
 
-  // Handlers for commands
-
-  respondToAtAll(res) {
-    const users = this.robot.brain.users();
-    const userNames = Object.values(users).map((user) => user.name);
-    const messageText = `Hey ${userNames.join(", ")}, check this out!`;
-
-    this.sendMessage(messageText);
-  }
-
-  respondToSchedule(res) {
-    const schedule = "Here's the schedule for the trip:\n1. Welcome Mixer and Night Swim \n2. DG Wine Tour \n3. Opulence Dinner \n4. Sunset Boatride ";
-    this.sendMessage(schedule);
-  }
-
-  respondToTodaySchedule(res) {
-    const todayFormatted = new Date().toISOString().split('T')[0];
-    const schedules = {
-      '2023-08-20': 'Here is the schedule for Day 1:\n1. Event A\n2. Event B\n3. Event C',
-      '2023-08-05': 'Here is the schedule for Day 2:\n1. Event X\n2. Event Y\n3. Event Z',
-      // Add more dates and schedules as needed
-    };
-    const schedule = schedules[todayFormatted] || 'There is no schedule available for today.';
-    this.sendMessage(schedule);
-  }
-
-  respondToTravelForm(res) {
-    const travelFormURL = "https://docs.google.com/forms/d/e/1FAIpQLSf9tJWxnH0NPq_oFB_a_bWwZq0zqf1AWDcBZ4tnvnxIsZB3qg/viewform";
-    const messageText = `Here's the link to the flight form: ${travelFormURL}`;
-    this.sendMessage(messageText);
-  }
-
   respondToHotel(res) {
     const hotelInfo = "Lisbon Marriott Hotel\nAddress: Avenida dos Combatentes 45, Lisbon 1600-042, Portugal";
-    const messageText = `Here's the information about Lisbon Marriott Hotel:\n${hotelInfo}`;
-    this.sendMessage(messageText);
-  }
+    
+    const message = {
+      text: `Here's the information about Lisbon Marriott Hotel:\n${hotelInfo}`,
+      bot_id,
+    };
 
-  respondToLookbook(res) {
-    const lookbookUrl = "https://drive.google.com/file/d/1WVCka6T2KG1seeskCAvYvZKrOFwusPmG/view";
-    const messageText = `Here's the lookbook URL: ${lookbookUrl}`;
-    this.sendMessage(messageText);
-  }
-
-  // Run the bot
-  run() {
-    this.commands.forEach(({ pattern, handler }) => {
-      this.robot.hear(pattern, (res) => handler.call(this, res));
+    const json = JSON.stringify(message);
+    const groupmeAPIOptions = {
+      agent: false,
+      host: "api.groupme.com",
+      path: "/v3/bots/post",
+      port: 443,
+      method: "POST",
+      headers: {
+        "Content-Length": json.length,
+        "Content-Type": "application/json",
+        "X-Access-Token": token,
+      },
+    };
+    const req = https.request(groupmeAPIOptions, (response) => {
+      let data = "";
+      response.on("data", (chunk) => (data += chunk));
+      response.on("end", () =>
+        console.log(`[GROUPME RESPONSE] ${response.statusCode} ${data}`)
+      );
     });
+    req.end(json);
+  }
+
+    respondToLookbook(res) {
+    const lookbookUrl = "https://drive.google.com/file/d/1WVCka6T2KG1seeskCAvYvZKrOFwusPmG/view";
+
+    const message = {
+      text: `Here's the lookbook URL: ${lookbookUrl}`,
+      bot_id,
+      attachments: [{ loci: [], type: 'mentions', user_ids: [] }],
+    };
+
+    const users = this.robot.brain.users();
+    Object.keys(users).forEach((userID, index) => {
+      if (this.blacklist.indexOf(userID) !== -1) return;
+      message.attachments[0].loci.push([index, index + 1]);
+      message.attachments[0].user_ids.push(userID);
+    });
+
+    const json = JSON.stringify(message);
+    const groupmeAPIOptions = {
+      agent: false,
+      host: 'api.groupme.com',
+      path: '/v3/bots/post',
+      port: 443,
+      method: 'POST',
+      headers: {
+        'Content-Length': json.length,
+        'Content-Type': 'application/json',
+        'X-Access-Token': token,
+      },
+    };
+    const req = https.request(groupmeAPIOptions, (response) => {
+      let data = '';
+      response.on('data', (chunk) => (data += chunk));
+      response.on('end', () =>
+        console.log(`[GROUPME RESPONSE] ${response.statusCode} ${data}`)
+      );
+    });
+    req.end(json);
+  }
+  
+  // Defines the main logic of the bot
+  run() {
+    // Register listeners with hubot
+    this.robot.hear(/get id (.+)/i, res => this.respondToID(res, res.match[1]));
+    this.robot.hear(/get name (.+)/i, res =>
+      this.respondToName(res, res.match[1])
+    );
+    this.robot.hear(/view( raw)* blacklist/i, res =>
+      this.respondToViewBlacklist(res)
+    );
+    this.robot.hear(/blacklist (.+)/i, res =>
+      this.respondToBlacklist(res, res.match[1])
+    );
+    this.robot.hear(/whitelist (.+)/i, res =>
+      this.respondToWhitelist(res, res.match[1])
+    );
+
+    // Mention @all command
+    this.robot.hear(/(.*)@all(.*)/i, res => this.respondToAtAll(res));
+    this.robot.hear(/@itinerary/i, (res) => this.respondToSchedule(res));
+    this.robot.hear(/@today/i, (res) => this.respondToTodaySchedule(res));
+    this.robot.hear(/@form/i, (res) => this.respondToTravelForm(res));
+    this.robot.hear(/@hotel/i, (res) => this.respondToHotel(res));
+    this.robot.hear(/@lookbook/i, (res) => this.respondToLookbook(res));
   }
 }
 
-module.exports = (robot) => {
-  const bot = new GroupMeBot(robot);
+module.exports = robot => {
+  const bot = new AllBot(robot);
   bot.run();
 };
+
